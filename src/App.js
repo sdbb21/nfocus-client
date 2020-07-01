ceca71b27bd82f6601b58c18dbb70b570bcb5845
Sebastian Baez <sdbb.unl@gmail.com>
2020-07-01 11:23:18 +0200

    Login/SignUp implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
+import MessageBox from "./components/MessageBox";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+import { getUserWithStoredToken } from "./store/user/actions";
+
 function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Navigation />
+      <MessageBox />
+      {isLoading ? <Loading /> : null}
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
