a999b6879be658acc2c74452a41e2719afa1968f
Sebastian Baez <sdbb.unl@gmail.com>
2020-07-10 13:08:06 +0200

    REACT_APP_ fix

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 console.log("PROCESS.ENV", process.env);
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
